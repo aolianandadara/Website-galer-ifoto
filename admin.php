4e8b8c701b4e3e87dd1f4be94d765c87b6eb3005
aolianandadara <aolianandadara@gmail.com>
2024-02-20 17:46:50 +0800

    tes

---
+++ admin.php
@@ -0,0 +1,21 @@
+</DOCTYPEPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>document</title>
+</head>
+<body>
+<h1>HALAMAN ADMIN</h1>
+<a href ="">tambah data</a>
+<table border=1>
+  <thead>
+     <tr>
+        <th>fitur</th>
+        <th>user</th>
+</tr>
+</thead>
+<body>
+</body>
+
+
